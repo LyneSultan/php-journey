4d117d885c7da59ff47e6cbe241f96c45c0352e2
LyneSultan <lynesultane@gmail.com>
2024-11-17 19:52:42 +0200

    create - linkedList class

--- linkedList.php
+++ linkedList.php
@@ -11,5 +11,31 @@ class Node{
     return $this->value;
   }
 }
-$node1 = new Node("test");
-echo $node1;
+class LinkedList{
+  public $head;
+
+  public function __construct(){
+    $this->head=NULL;
+  }
+  public function addNode($value){
+    $node=new Node($value);
+
+    if($this->head){
+      $traversale_node=$this->head;
+
+      while($traversale_node!==Null){
+        $traversale_node=$traversale_node->next;
+      }
+
+      $traversale_node->next=$node;
+    }else{
+      $this->head=$node;
+    }
+  }
+  public function __toString() {
+    return $this->head->value;
+  }
+}
+$test = new linkedList();
+$test->addNode("hello");
+echo $test;
