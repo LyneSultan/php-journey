7da47282e52d49ce0bdf4aa209971ea435469afd
LyneSultan <lynesultane@gmail.com>
2024-11-17 20:05:02 +0200

    add - print method

--- linkedList.php
+++ linkedList.php
@@ -32,10 +32,29 @@ class LinkedList{
       $this->head=$node;
     }
   }
-  public function __toString() {
-    return $this->head->value;
+  public function countVowels($str) {
+    $vowels = ['a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'];
+    $count = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], $vowels)) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+  public function print() {
+    $traversal_node=$this->head;
+
+    while($traversal_node!==NULL){
+
+      if($this->countVowels($traversal_node->value)==2){
+        echo $traversal_node->value;
+      }
+      $traversal_node = $traversal_node->next;
+    }
   }
 }
 $test = new linkedList();
-$test->addNode("hello");
-echo $test;
+$test->addNode("hele");
+$test->print();
